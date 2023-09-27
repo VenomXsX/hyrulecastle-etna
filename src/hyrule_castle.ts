cedc44bac415c46ba040dd920ac20bbf4468a487
TheMysteryz <zhuroland@hotmail.com>
2023-09-27 13:26:11 +0200

    renamed files

--- src/hyrule_castle.ts
+++ src/hyrule_castle.ts
@@ -1,6 +1,6 @@
 import gameInit from '~/lib/inits/game_init';
 import { outputText } from './utils/helper';
-import chooseGameMode from './lib/chooseGameMode';
+import chooseGameMode from './lib/choose_game_mode';
 
 function main() {
 	outputText('./src/lib/text/welcome_menu.txt');
