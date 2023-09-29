f1f81b7ffa91c6dc6f8e7e8878ec9073a16d0a61
TheMysteryz <zhuroland@hotmail.com>
2023-09-29 16:25:01 +0200

    handle quantity

--- src/lib/handle_object.ts
+++ src/lib/handle_object.ts
@@ -1,5 +1,22 @@
+import { Item } from '../types/type';
+
 export const searchObjById = <T>(obj: T[], id: number) =>
 	obj.filter((item: any) => item.id === id)[0];
 
 export const searchObjByName = <T>(obj: T[], name: string) =>
 	obj.filter((item: any) => item.name === name)[0];
+
+export const handleQuantity = (
+	obj: Item,
+	option: 'add' | 'remove',
+	value: number,
+) => {
+	switch (option) {
+		case 'add':
+			obj.quantity += value;
+			break;
+		case 'remove':
+			obj.quantity -= obj.quantity > 0 ? value : 0;
+			break;
+	}
+};
